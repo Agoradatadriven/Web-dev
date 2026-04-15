dad01fffcce457898f17a2fabc5aa5e5ae9666dc
Agora Data Driven <info@agoradatadriven.com>
2026-04-15 15:21:25 +0800

    Fix navbar logo path for GitHub Pages

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -37,7 +37,11 @@ export default function Navbar() {
     >
       <div className="max-w-7xl mx-auto px-6 md:px-12 flex items-center justify-between">
         <a href="#" className="block">
-          <img src="/agora-logo-nav.png" alt="Agora Data Driven" className="h-10 md:h-12 w-auto object-contain" />
+          <img
+            src={`${import.meta.env.BASE_URL}agora-logo-nav.png`}
+            alt="Agora Data Driven"
+            className="h-10 md:h-12 w-auto object-contain"
+          />
         </a>
 
         {/* Desktop Nav */}
